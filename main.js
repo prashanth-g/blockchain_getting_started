5da59827e6eed2a394ad6a4aa27c6c3bf8a47a31
prashanth-g <prashanthcsgce@gmail.com>
2019-07-28 17:13:46 +0530

    Add Blockchain class and required methods

--- main.js
+++ main.js
@@ -1,13 +1,41 @@
+const SHA26 = require('crypto-js/sha256')
+
 class Block {
     constructor(index, timestamp, data, previousHash = '') {
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
-        this.hash = '';
+        this.hash = this.calculateHash();
     }
 
     calculateHash() {
-        // TODO: Add hashing
+        return SHA26(this.index + this.timestamp + this.previousHash + JSON.stringify(this.data)).toString();
     }
 }
+
+class BlockChain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, '28/07/2019', "Genesis block", "0");
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+
+let blockChain = new BlockChain();
+blockChain.addBlock(new Block(1, '28/07/2019', { amount:7 }));
+blockChain.addBlock(new Block(2, '28/07/2019', { amount:10 }));
+
+console.log(JSON.stringify(blockChain, null, 5));

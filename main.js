f618fda63d2decc6615d9b1b23853ace07cf7ed4
prashanth-g <prashanthcsgce@gmail.com>
2019-07-29 19:15:59 +0530

    Added blockchain integrity check

--- main.js
+++ main.js
@@ -32,6 +32,23 @@ class BlockChain {
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid() {
+        for(let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if(currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
 
 let blockChain = new BlockChain();
@@ -39,3 +56,5 @@ blockChain.addBlock(new Block(1, '28/07/2019', { amount:7 }));
 blockChain.addBlock(new Block(2, '28/07/2019', { amount:10 }));
 
 console.log(JSON.stringify(blockChain, null, 5));
+console.log('\nIs blockchian valid: ' + blockChain.isChainValid());
+
